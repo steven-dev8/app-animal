0364bf408a3a4e401d332ad48876db4cee4330e8
Stevenn <stevenndev8@gmail.com>
2025-03-08 18:09:16 -0300

    CORS policy now allows all links

--- src/server.py
+++ src/server.py
@@ -7,11 +7,9 @@ from random import randint
 storage_db = {}
 app = FastAPI()
 
-origins = ["http://localhost:8080"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
