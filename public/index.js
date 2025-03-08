82bd7459d5a52c3fbd33cf99e22230e91ec559e6
Stevenn <stevenndev8@gmail.com>
2025-03-08 18:10:49 -0300

    dynamic list implemented and async function

--- public/index.js
+++ public/index.js
@@ -1,5 +1,13 @@
-function app() {
-    console.log('App inicialized')
+async function listar_animais() {
+    const content = await axios.get('http://127.0.0.1:8000/animals') // Endpoint
+    const animais = content.data
+    
+    Object.values(animais).forEach(valor => {
+        const lista = document.getElementById('lista-animais')
+        const item = document.createElement('li')
+        item.innerText = valor.name
+        lista.appendChild(item)
+    })
 }
 
-app()
+listar_animais()
